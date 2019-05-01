058b70cafd9f04182f4e70c371619db0b0770323
CRUA003 <alexandre.cruel@orange.fr>
2019-05-01 16:27:39 -0400

    comparing # of lines

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 file = open('foo.txt', 'r')
+second = open('stud2.txt', 'r')
+
 n = 0
 for line in file:
     n += 1
 
-print("Le fichier fait ", n, "lignes")
+m = 0
+for line in second:
+    m += 1
+
+if m == n:
+    print("ATTENTION : Les deux fichiers ont le même nombre de lignes")
+else:
+    print("Les deux fichiers n'ont pas le même nombre de lignes")
