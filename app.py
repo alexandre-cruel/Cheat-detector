01d6496e330a45e1320b89b17db7602f49c4be24
CRUA003 <alexandre.cruel@orange.fr>
2019-05-03 15:00:33 -0400

    comparing # of words

--- app.py
+++ app.py
@@ -2,10 +2,9 @@ file = open('foo.txt', 'r')
 second = open('stud2.txt', 'r')
 
 n = 0
+m = 0
 for line in file:
     n += 1
-
-m = 0
 for line in second:
     m += 1
 
@@ -13,3 +12,20 @@ if m == n:
     print("ATTENTION : Les deux fichiers ont le même nombre de lignes")
 else:
     print("Les deux fichiers n'ont pas le même nombre de lignes")
+
+str_file = str(file)
+str_second = str(second)
+
+count1 = 0
+count2 = 0
+for word in str_file:
+    count1 += 1
+for word in str_second:
+    count1 += 1
+diff = count2 - count1
+print("Les fichiers ont", diff, "mots de différence.")
+
+
+packed_file = str_file.replace(' ', '')
+packed_second = str_second.replace(' ', '')
+
