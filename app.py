376ddd56a3900d7ea63179ed5ad6de06235faabc
CRUA003 <alexandre.cruel@orange.fr>
2019-05-09 16:25:57 -0400

    added basic lcs

--- app.py
+++ app.py
@@ -1,28 +1,11 @@
-
-# -------------- compte le nombre de lignes -------------- #
-ligne = open('foo.txt', 'r')
-ligne_deux = open('stud2.txt', 'r')
-
-n = 0
-m = 0
-for line in ligne:
-    n += 1
-for line in ligne_deux:
-    m += 1
-
-if m == n:
-    print("ATTENTION : Les deux fichiers ont le même nombre de lignes")
-else:
-    print("Les deux fichiers n'ont pas le même nombre de lignes")
-
-# -------------- compte le nombre de mots -------------- #
-
-with open('foo.txt', 'r') as file:
-    data = file.read()
-with open('stud2.txt', 'r') as file:
-    data2 = file.read()
-
-print("Les fichiers comprennent respectivement {} et {} mots".format(len(data.split()), len(data2.split())))
-
 # -------------- Comparaison -------------- #
+
+
+def lcs(x, y, m, n):    # lcs stands for longest common sequence
+    if m == 0 or n == 0:
+        return 0
+    elif x[m-1] == y[n-1]:
+        return 1+lcs(x, y, m-1, n-1)
+    else:
+        return max(lcs(x, y, m, n-1), lcs(x, y, m-1, n))
 
---
+++ main.py
@@ -0,0 +1,31 @@
+import app
+
+# -------------- compte le nombre de lignes -------------- #
+ligne = open('foo.txt', 'r')
+ligne_deux = open('stud2.txt', 'r')
+
+n = 0
+m = 0
+for line in ligne:
+    n += 1
+for line in ligne_deux:
+    m += 1
+
+if m == n:
+    print("ATTENTION : Les deux fichiers ont le même nombre de lignes")
+else:
+    print("Les deux fichiers n'ont pas le même nombre de lignes")
+
+# -------------- compte le nombre de mots -------------- #
+
+with open('foo.txt', 'r') as file:
+    data = file.read()
+with open('stud2.txt', 'r') as file:
+    data2 = file.read()
+
+print("Les fichiers comprennent respectivement {} et {} mots".format(len(data.split()), len(data2.split())))
+
+# --------------    MAIN     -------------- #
+
+app.lcs(data, data2, len(data), len(data2))
+
